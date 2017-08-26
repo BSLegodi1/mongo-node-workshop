b80e01e0642f8c10713279e381dbedaf1795b333
Simon Legodi <bslegodi@gmail.com>
2017-08-26 16:25:50 +0100

    using mangodb

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -3,7 +3,47 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser')
 
+const ObjectId = require('mongodb').ObjectId;
+const MongoClient = require('mongodb').MongoClient;
 router.use(bodyParser.json());
+router.get('/students', (req, res) => {
+    const mongoConnection = 'mongodb://localhost:27017/profile';
+
+    MongoClient.connect(mongoConnection, (err, db) => {
+        const cursor = db.collection('students').find({});
+        cursor.toArray((error, students) => {
+            db.close();
+            res.json(students);
+        });
+    });
+});
+
+
+router.get('/posts', (req, res) => {
+    const mongoConnection = 'mongodb://localhost:27017/test';
+
+    MongoClient.connect(mongoConnection, (err, db) => {
+        const cursor = db.collection('posts').find({});
+        cursor.toArray((error, posts) => {
+            db.close();
+            res.json(posts);
+        });
+    });
+});
+
+
+
+router.get('/students/:id', (req, res) => {
+    const mongoConnection = 'mongodb://localhost:27017/profile';
+    const studentId = req.params.id
+    MongoClient.connect(mongoConnection, (err, db) => {
+        const cursor = db.collection('students').find(studentId);
+        cursor.toArray((error, students) => {
+            db.close();
+            res.json(students);
+        });
+    });
+});
 
 router.get('/posts', (req, res, next) => {
     const filePath = __dirname + '/../data/posts.json';
